5f3510154e677eb411914b4178eceb43a0b97a71
Lukas Schreiber <info@lukasschreiber.com>
2023-04-01 21:41:10 +0200

    bundler uses cache

--- server/bundler.ts
+++ server/bundler.ts
@@ -7,17 +7,7 @@ interface BundleFile {
 
 export const getBundleFor = (fileName: string): Set<BundleFile> => {
     fileName = fileName.replace(/\\/g, "/");
-    const file: BundleFile | null = (() => {
-        try {
-            return { data: fs.readFileSync(`${process.env.LDRAW_LIB}/parts/${fileName}`), name: fileName }
-        } catch (e) {
-            try {
-                return { data: fs.readFileSync(`${process.env.LDRAW_LIB}/p/${fileName}`), name: fileName }
-            } catch (e) {
-                return null;
-            }
-        }
-    })();
+    const file = getFile(fileName)
 
     if (file === null) return new Set();
 
@@ -28,4 +18,22 @@ export const getBundleFor = (fileName: string): Set<BundleFile> => {
     }));
 
     return new Set([file, ...Array.from(subfiles).map(f => getBundleFor(f)).reduce((acc, x) => new Set([...acc, ...x]), new Set())]);
+}
+
+export const getPrebuildBundleFor = (id: string) => {
+    const file = `${process.env.ZIP_DIR}/${id}.zip`;
+    if (!fs.existsSync(file)) return null;
+    return fs.createReadStream(file);
+}
+
+const getFile = (fileName: string): BundleFile | null => {
+    try {
+        return { data: fs.readFileSync(`${process.env.LDRAW_LIB}/parts/${fileName}`), name: fileName }
+    } catch (e) {
+        try {
+            return { data: fs.readFileSync(`${process.env.LDRAW_LIB}/p/${fileName}`), name: fileName }
+        } catch (e) {
+            return null;
+        }
+    }
 }
--- server/index.ts
+++ server/index.ts
@@ -1,15 +1,32 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { getBundleFor } from './bundler';
+import { getBundleFor, getPrebuildBundleFor } from './bundler';
 import JSZip from "jszip";
+import fs from "fs";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+if (!fs.existsSync(process.env.ZIP_DIR ?? "./zip")) {
+    fs.mkdirSync(process.env.ZIP_DIR ?? "./zip");
+}
+
 app.get('/bundle/:id', async (req: Request, res: Response) => {
+    const zipName = `${req.params.id}.zip`;
     const entryFileName = `${req.params.id}.dat`;
+
+    const prebuildBundle = getPrebuildBundleFor(req.params.id);
+    if (prebuildBundle !== null) {
+        res.writeHead(200, {
+            'Content-Type': 'application/zip',
+            'Content-disposition': `attachment; filename=${zipName}`
+        });
+
+        return prebuildBundle.pipe(res);
+    }
+
     const bundle = Array.from(getBundleFor(entryFileName));
 
     if (bundle.length > 0 && req.params.id !== null) {
@@ -18,11 +35,15 @@ app.get('/bundle/:id', async (req: Request, res: Response) => {
 
         res.writeHead(200, {
             'Content-Type': 'application/zip',
-            'Content-disposition': `attachment; filename=${req.params.id}.zip`
+            'Content-disposition': `attachment; filename=${zipName}`
         });
 
+        // if storage space is a concern comment this out and all bundles are calculated on the fly
         zip.generateNodeStream({ streamFiles: true })
-            .pipe(res);
+            .pipe(fs.createWriteStream(`${process.env.ZIP_DIR}/${zipName}`));
+
+        zip.generateNodeStream({ streamFiles: true })
+            .pipe(res)
     } else {
         res.sendStatus(404)
     }
