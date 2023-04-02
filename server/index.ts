96ead43cac76222c7044b66d3e0721c0545db44a
Lukas Schreiber <info@lukasschreiber.com>
2023-04-02 17:39:07 +0200

    serve ldraw files static

--- server/index.ts
+++ server/index.ts
@@ -1,60 +1,21 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
-import { getBundleFor, getPrebuildBundleFor } from './bundler';
-import JSZip from "jszip";
-import fs from "fs";
 import cors from "cors";
+import { LDRAWRouter } from './ldraw-resources/static-files';
 
 dotenv.config();
-
-if (!fs.existsSync(process.env.ZIP_DIR ?? "./zip")) {
-    fs.mkdirSync(process.env.ZIP_DIR ?? "./zip");
-}
 
 const app: Express = express();
 const port = process.env.PORT;
 
 app.use(cors())
 
+app.use("/ldraw", LDRAWRouter)
+
 app.get('/', (req: Request, res: Response) => {
     res.send("test")
 })
 
-app.get('/bundle/:id', async (req: Request, res: Response) => {
-    const zipName = `${req.params.id}.zip`;
-    const entryFileName = `${req.params.id}.dat`;
-
-    const zipHeaders = {
-        'Content-Type': 'application/zip',
-        'Content-Disposition': 'inline',
-        // 'Content-disposition': `attachment; filename=${zipName}`
-    }
-
-    const prebuildBundle = getPrebuildBundleFor(req.params.id);
-    if (prebuildBundle !== null) {
-        res.writeHead(200, zipHeaders);
-
-        return prebuildBundle.pipe(res);
-    }
-
-    const bundle = Array.from(getBundleFor(entryFileName));
-
-    if (bundle.length > 0 && req.params.id !== null) {
-        const zip = new JSZip();
-        bundle.forEach(file => zip.file(file.name, file.data));
-
-        res.writeHead(200, zipHeaders);
-
-        // if storage space is a concern comment this out and all bundles are calculated on the fly
-        zip.generateNodeStream({ streamFiles: true })
-            .pipe(fs.createWriteStream(`${process.env.ZIP_DIR}/${zipName}`));
-
-        zip.generateNodeStream({ streamFiles: true })
-            .pipe(res)
-    } else {
-        res.sendStatus(404)
-    }
-});
 
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
--- src/parser/part.rs
+++ src/parser/part.rs
@@ -1,18 +1,11 @@
-use reqwest;
-use std::io::Cursor;
-type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::{Request, RequestInit, RequestMode, Response};
 
-async fn download_zip(url: String) -> Result<()> {
-    let response = reqwest::get(url).await?;
-    let content = Cursor::new(response.bytes().await?);
-    log::info!("{:?}", content);
+pub async fn test(id: &str) -> Result<(), JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
+
     Ok(())
-}
-
-pub async fn test(id: &str) {
-    download_zip(format!("{}{}", "http://localhost:3000/bundle/", id))
-        .await
-        .unwrap();
-
-    log::info!("parsing part {}", id);
 }
