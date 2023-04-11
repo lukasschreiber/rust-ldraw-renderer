6b2c4c7aabfcf0aff460c132b64913453b641c1d
Lukas Schreiber <info@lukasschreiber.com>
2023-04-11 20:09:40 +0200

    parse things

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,2 @@
 pub mod part;
+pub mod tokenizer;
--- src/parser/part.rs
+++ src/parser/part.rs
@@ -1,275 +1,170 @@
-use chrono::NaiveDate;
-use std::str::FromStr;
-use three_d::{vec3, Matrix3, Vector3};
+use crate::parser::tokenizer::*;
+use three_d::{Matrix3, Vector3};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::JsFuture;
 use web_sys::{Request, RequestInit, RequestMode, Response};
 
-#[derive(Debug)]
-struct Color {
-    value: u32,
+#[derive(Debug, Clone)]
+pub struct LDrawAuthor {
+    name: String,
+    username: Option<String>,
 }
 
-// make traits and extend traits
-#[derive(Debug)]
-enum LDrawCommand {
-    Comment,
-    Title(String),
-    Name(String),
-    Author(String, Option<String>),
-    License(String, String),
-    LDrawOrg(LDrawType),
-    Category(String),
-    Keywords(Vec<String>),
-    History(NaiveDate, Option<String>, String),
-    BFCCertification(Option<BFCDirection>),
-    SubfileReference(Color, Vector3<f32>, Matrix3<f32>, String),
-    Contour(Color, Vector3<f32>, Vector3<f32>),
-    Triangle(Color, Vector3<f32>, Vector3<f32>, Vector3<f32>),
-    Quadrilateral(
-        Color,
-        Vector3<f32>,
-        Vector3<f32>,
-        Vector3<f32>,
-        Vector3<f32>,
-    ),
-    OptionalContour(
-        Color,
-        Vector3<f32>,
-        Vector3<f32>,
-        Vector3<f32>,
-        Vector3<f32>,
-    ),
+#[derive(Debug, Clone)]
+pub struct LDrawContour {
+    color: Color,
+    x: Vector3<f32>,
+    y: Vector3<f32>,
 }
 
-#[derive(Debug, PartialEq)]
-enum BFCDirection {
-    CW,
-    CCW,
+#[derive(Debug, Clone)]
+pub struct LDrawOptionalContour {
+    color: Color,
+    x: Vector3<f32>,
+    y: Vector3<f32>,
+    ox: Vector3<f32>,
+    oy: Vector3<f32>,
 }
 
-impl FromStr for BFCDirection {
-    type Err = ();
-    fn from_str(input: &str) -> Result<BFCDirection, Self::Err> {
-        match input {
-            "CW" => Ok(BFCDirection::CW),
-            "CCW" => Ok(BFCDirection::CCW),
-            _ => Err(()),
-        }
-    }
+#[derive(Debug, Clone)]
+pub struct LDrawTriangle {
+    color: Color,
+    x: Vector3<f32>,
+    y: Vector3<f32>,
+    z: Vector3<f32>,
 }
 
-#[derive(Debug, PartialEq)]
-enum LDrawType {
-    Part,
-    Subpart,
-    Primitive,
-    Primitive8,
-    Primitive48,
-    Shortcut,
-    UnofficialPart,
-    UnofficialSubpart,
-    UnofficialPrimitive,
-    UnofficialPrimitive8,
-    UnofficialPrimitive48,
-    UnofficialShortcut,
+#[derive(Debug, Clone)]
+pub struct LDrawSubfile {
+    color: Color,
+    bfc_direction: BFCDirection,
+    transformation: Matrix3<f32>,
+    translation: Vector3<f32>,
+    filename: String,
 }
 
-impl FromStr for LDrawType {
-    type Err = ();
-    fn from_str(input: &str) -> Result<LDrawType, Self::Err> {
-        match input {
-            "Part" => Ok(LDrawType::Part),
-            "Subpart" => Ok(LDrawType::Subpart),
-            "Primitive" => Ok(LDrawType::Primitive),
-            "8_Primitive" => Ok(LDrawType::Primitive8),
-            "48_Primitive" => Ok(LDrawType::Primitive48),
-            "Shortcut" => Ok(LDrawType::Shortcut),
-            "Unofficial_Part" => Ok(LDrawType::UnofficialPart),
-            "Unofficial_Subpart" => Ok(LDrawType::UnofficialSubpart),
-            "Unofficial_Primitive" => Ok(LDrawType::UnofficialPrimitive),
-            "Unofficial_8_Primitive" => Ok(LDrawType::UnofficialPrimitive8),
-            "Unofficial_48_Primitive" => Ok(LDrawType::UnofficialPrimitive48),
-            "Unofficial_Shortcut" => Ok(LDrawType::UnofficialShortcut),
-            _ => Err(()),
-        }
-    }
+#[derive(Debug, Clone)]
+pub struct LDrawFile {
+    name: String,
+    title: String,
+    author: LDrawAuthor,
+    bfc_direction: BFCDirection,
+    lines: Vec<LDrawContour>,
+    optional_lines: Vec<LDrawOptionalContour>,
+    triangles: Vec<LDrawTriangle>,
+    subfiles: Vec<LDrawSubfile>,
 }
 
 pub async fn parse_part(id: &str) -> Result<(), JsValue> {
-    let lines = parse_file(&format!("{}.dat", id)).await?;
+    let files = get_bundle_lst(&format!("{}", id)).await?;
 
-    for line in &lines {
-        log::info!("{:?}", line);
+    log::info!("{:?}", files);
+
+    for file in files {
+        let lines = get_subfile(&file).await?;
+        let tokens = tokenize_file(lines).await?;
+
+        let mut file = LDrawFile {
+            name: String::new(),
+            title: String::new(),
+            author: LDrawAuthor {
+                name: String::new(),
+                username: None,
+            },
+            bfc_direction: BFCDirection::CW,
+            lines: Vec::new(),
+            optional_lines: Vec::new(),
+            triangles: Vec::new(),
+            subfiles: Vec::new(),
+        };
+
+        for token in &tokens {
+            match token {
+                Some(LDrawCommand::Name(name)) => file.name = name.to_string(),
+                Some(LDrawCommand::Title(title)) => file.title = title.to_string(),
+                Some(LDrawCommand::Author(name, username)) => {
+                    file.author.name = name.to_string();
+                    file.author.username = username.clone()
+                }
+                Some(LDrawCommand::BFCCertification(direction)) => {
+                    if direction.is_some() {
+                        file.bfc_direction = direction.clone().unwrap();
+                    }
+                }
+                Some(LDrawCommand::Contour(color, x, y)) => file.lines.push(LDrawContour {
+                    color: color.clone(),
+                    x: x.clone(),
+                    y: y.clone(),
+                }),
+                Some(LDrawCommand::OptionalContour(color, x, y, ox, oy)) => {
+                    file.optional_lines.push(LDrawOptionalContour {
+                        color: color.clone(),
+                        x: x.clone(),
+                        y: y.clone(),
+                        ox: ox.clone(),
+                        oy: oy.clone(),
+                    })
+                }
+                Some(LDrawCommand::Triangle(color, x, y, z)) => {
+                    file.triangles.push(LDrawTriangle {
+                        color: color.clone(),
+                        x: x.clone(),
+                        y: y.clone(),
+                        z: z.clone(),
+                    })
+                }
+                Some(LDrawCommand::Quadrilateral(color, x, y, z, w)) => {
+                    file.triangles.push(LDrawTriangle {
+                        color: color.clone(),
+                        x: x.clone(),
+                        y: y.clone(),
+                        z: z.clone(),
+                    });
+                    file.triangles.push(LDrawTriangle {
+                        color: color.clone(),
+                        x: z.clone(),
+                        y: w.clone(),
+                        z: x.clone(),
+                    })
+                }
+                Some(LDrawCommand::SubfileReference(
+                    color,
+                    translation,
+                    transformation,
+                    filename,
+                )) => file.subfiles.push(LDrawSubfile {
+                    color: color.clone(),
+                    bfc_direction: file.bfc_direction.clone(),
+                    translation: translation.clone(),
+                    transformation: transformation.clone(),
+                    filename: filename.to_string(),
+                }),
+                _ => {}
+            }
+        }
+
+        log::info!("{:?}", file);
     }
 
     Ok(())
 }
 
-async fn parse_file(file_name: &str) -> Result<Vec<Option<LDrawCommand>>, JsValue> {
-    let lines = get_subfile(file_name).await?;
-    let parsed_lines: Vec<Option<LDrawCommand>> = lines
-        .iter()
-        .enumerate()
-        .map(|(i, line)| parse_line(line.to_string(), i))
-        .filter(|p| p.is_some())
-        .collect();
+async fn get_bundle_lst(id: &str) -> Result<Vec<String>, JsValue> {
+    let mut opts = RequestInit::new();
+    opts.method("GET");
+    opts.mode(RequestMode::Cors);
 
-    Ok(parsed_lines)
-}
+    let url = format!("http://localhost:3000/ldraw/bundle/{}.lst", id);
+    let request = Request::new_with_str_and_init(&url, &opts)?;
 
-fn parse_line(line: String, index: usize) -> Option<LDrawCommand> {
-    if line.trim().len() <= 1 {
-        return None;
-    }
+    let window = web_sys::window().unwrap();
+    let response_value = JsFuture::from(window.fetch_with_request(&request)).await?;
+    let response: Response = response_value.dyn_into().unwrap();
 
-    let tokens: Vec<&str> = line.split_whitespace().collect();
-    let tail = tokens.split_first().unwrap().1;
-    match tokens[0] {
-        "0" => Some(parse_meta(tail.to_vec(), index)),
-        "1" => Some(parse_subfile_reference(tail.to_vec())),
-        "2" => Some(parse_contour(tail.to_vec())),
-        "3" => Some(parse_triangle(tail.to_vec())),
-        "4" => Some(parse_quadrilateral(tail.to_vec())),
-        "5" => Some(parse_optional_contour(tail.to_vec())),
-        _ => None,
-    }
-}
+    let text = JsFuture::from(response.text()?).await?.as_string().unwrap();
+    let files = text.lines().map(|line| line.to_string()).collect();
 
-fn parse_optional_contour(tokens: Vec<&str>) -> LDrawCommand {
-    let color = parse_color(tokens[0]);
-    let x = parse_vec3(tokens[1..4].to_vec());
-    let y = parse_vec3(tokens[4..7].to_vec());
-    let z = parse_vec3(tokens[7..10].to_vec());
-    let w = parse_vec3(tokens[10..13].to_vec());
-
-    LDrawCommand::OptionalContour(color, x, y, z, w)
-}
-
-fn parse_quadrilateral(tokens: Vec<&str>) -> LDrawCommand {
-    let color = parse_color(tokens[0]);
-    let x = parse_vec3(tokens[1..4].to_vec());
-    let y = parse_vec3(tokens[4..7].to_vec());
-    let z = parse_vec3(tokens[7..10].to_vec());
-    let w = parse_vec3(tokens[10..13].to_vec());
-
-    LDrawCommand::Quadrilateral(color, x, y, z, w)
-}
-
-fn parse_triangle(tokens: Vec<&str>) -> LDrawCommand {
-    let color = parse_color(tokens[0]);
-    let x = parse_vec3(tokens[1..4].to_vec());
-    let y = parse_vec3(tokens[4..7].to_vec());
-    let z = parse_vec3(tokens[7..10].to_vec());
-
-    LDrawCommand::Triangle(color, x, y, z)
-}
-
-fn parse_contour(tokens: Vec<&str>) -> LDrawCommand {
-    let color = parse_color(tokens[0]);
-    let x = parse_vec3(tokens[1..4].to_vec());
-    let y = parse_vec3(tokens[4..7].to_vec());
-
-    LDrawCommand::Contour(color, x, y)
-}
-
-fn parse_subfile_reference(tokens: Vec<&str>) -> LDrawCommand {
-    let color = parse_color(tokens[0]);
-    let file = parse_file_name(tokens[13]);
-
-    let translation = parse_vec3(tokens[1..4].to_vec());
-    let transformation = parse_mat3(tokens[4..13].to_vec());
-
-    LDrawCommand::SubfileReference(color, translation, transformation, file)
-}
-
-fn parse_vec3(tokens: Vec<&str>) -> Vector3<f32> {
-    let tokens: Vec<f32> = tokens.iter().map(|token| token.parse().unwrap()).collect();
-    vec3(tokens[0], tokens[1], tokens[2])
-}
-
-fn parse_mat3(tokens: Vec<&str>) -> Matrix3<f32> {
-    let tokens: Vec<f32> = tokens.iter().map(|token| token.parse().unwrap()).collect();
-    Matrix3::new(
-        tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5], tokens[6], tokens[7],
-        tokens[8],
-    )
-}
-
-fn parse_color(token: &str) -> Color {
-    Color {
-        value: token.parse().unwrap(),
-    }
-}
-
-fn parse_file_name(token: &str) -> String {
-    token.replace("\\", "/")
-}
-
-fn parse_license(tokens: Vec<&str>) -> LDrawCommand {
-    LDrawCommand::License(
-        tokens[0..tokens.len() - 3].join(" "),
-        tokens.last().unwrap().to_string(),
-    )
-}
-
-fn parse_user_name(token: &str) -> Option<String> {
-    if token.starts_with("[") && token.ends_with("]") {
-        Some(token[1..token.len() - 1].to_string())
-    } else {
-        None
-    }
-}
-
-fn parse_author(tokens: Vec<&str>) -> LDrawCommand {
-    let user_name_option = tokens.split_last().unwrap().0;
-    let user_name = parse_user_name(user_name_option);
-
-    let real_name = if user_name.is_none() {
-        tokens.join(" ")
-    } else {
-        tokens.split_last().unwrap().1.join(" ")
-    };
-    LDrawCommand::Author(real_name, user_name)
-}
-
-fn parse_history(tokens: Vec<&str>) -> LDrawCommand {
-    let date = NaiveDate::parse_from_str(tokens[0], "%Y-%m-%d");
-    let user_name = parse_user_name(tokens[1]);
-    let text = tokens.split_at(2).1.join(" ");
-    LDrawCommand::History(date.unwrap(), user_name, text)
-}
-
-// 0 BFC ( CERTIFY ( CCW | CW ) | NOCERTIFY )
-fn parse_bfc_certification(tokens: Vec<&str>) -> LDrawCommand {
-    match tokens[0] {
-        "CERTIFY" => LDrawCommand::BFCCertification(BFCDirection::from_str(tokens[1]).ok()),
-        "NOCERTIFY" => LDrawCommand::BFCCertification(None),
-        _ => LDrawCommand::Comment,
-    }
-}
-
-fn parse_meta(tokens: Vec<&str>, line_index: usize) -> LDrawCommand {
-    if line_index == 0 {
-        LDrawCommand::Title(tokens.join(" "))
-    } else {
-        let tail = tokens.split_first().unwrap().1;
-        match tokens[0] {
-            "Name:" => LDrawCommand::Name(tail.join(" ")),
-            "Author:" => parse_author(tail.to_vec()),
-            "!LICENSE" => parse_license(tail.to_vec()),
-            "!LDRAW_ORG" => LDrawCommand::LDrawOrg(LDrawType::from_str(tail[0]).unwrap()),
-            "!CATEGORY" => LDrawCommand::Category(tail[0].to_string()),
-            "!KEYWORDS" => LDrawCommand::Keywords(
-                tail.iter()
-                    .map(|s| s.to_string().replace(",", ""))
-                    .collect(),
-            ),
-            "!HISTORY" => parse_history(tail.to_vec()),
-            "BFC" => parse_bfc_certification(tail.to_vec()),
-            _ => LDrawCommand::Comment,
-        }
-    }
+    Ok(files)
 }
 
 async fn get_subfile(name: &str) -> Result<Vec<String>, JsValue> {
